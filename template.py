32af15e6e2a61794cbb9522966c6ec31f566611a
mrjulian <72151113+mrjulian@users.noreply.github.com>
2020-12-17 10:37:29 -0700

    Create template.py
    Write code in this file.

---
+++ template.py
@@ -0,0 +1,7 @@
+# declare variables that will be used for the madlib
+
+
+# create the input options for user to know the prompt
+
+
+# create the madlib text with the appropriate string variables included so that it will run with a single print statement
